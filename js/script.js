dd131f6c899dbc13c5804f60573d7a9f346753ae
amanokosuke <amanokousuke1011@gmail.com>
2021-11-01 03:45:02 +0900

    ver1.0

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+    $('.js-modal-open').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
+    });
+
+    $(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 1300;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+});
